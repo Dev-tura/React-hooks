809c9156e15aa3929affa5c5d31c117cbcb65512
dev-tura <adelajatura@gmail.com>
2023-02-04 02:12:48 +0100

    React-hooks

---
+++ File-uploader/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Uploader from "./Components/Uploader";
+
+function App() {
+  return (
+    <div className="App">
+      <uploader />
+    </div>
+  );
+}
+
+export default App;
---
+++ File-uploader/src/Components/Uploader.js
@@ -0,0 +1,14 @@
+import react from "react";
+import { useState, useRef } from "react";
+
+const Uploader = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="file" id="file-upload" accept="image/*" />
+      </form>
+    </div>
+  );
+};
+
+export default Uploader;
